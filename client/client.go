22501a9969ae1a3a561688679f20af005a07f13d
Leonardo Diniz <leonardodiber@gmail.com>
2023-04-17 20:15:37 -0300

    fix(server,client): adds the posibility to log when we get an error due to the context timeout being reached

--- client/client.go
+++ client/client.go
@@ -16,46 +16,53 @@ func RequestBid() error {
 	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Millisecond)
 	defer cancel()
 
-	client := &http.Client{}
-	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
-	if err != nil {
-		return err
+	select {
+	case <-ctx.Done():
+		fmt.Printf("%s in RequestBid\n", ctx.Err())
+		return ctx.Err()
+	default:
+
+		client := &http.Client{}
+		req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+		if err != nil {
+			return err
+		}
+
+		res, err := client.Do(req)
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+		defer res.Body.Close()
+
+		responseData, err := io.ReadAll(res.Body)
+		if err != nil {
+			return err
+		}
+
+		var bid string
+		err = json.Unmarshal(responseData, &bid)
+		if err != nil {
+			return err
+		}
+
+		cotacao := fmt.Sprintf("Dolar: %s", bid)
+
+		file, err := os.OpenFile("cotacao.txt", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+		if err != nil {
+			fmt.Println("Um erro ocorreu ao tentar abrir/criar o arquivo")
+			return err
+		}
+		defer file.Close()
+
+		_, err = file.WriteString(cotacao)
+		if err != nil {
+			fmt.Println("Um erro ocorreu ao tentar salvar os dados no arquivo")
+			return err
+		}
+
+		fmt.Printf("Cotacao salva no arquivo com sucesso!\n")
+
+		return nil
 	}
-
-	res, err := client.Do(req)
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-	defer res.Body.Close()
-
-	responseData, err := io.ReadAll(res.Body)
-	if err != nil {
-		return err
-	}
-
-	var bid string
-	err = json.Unmarshal(responseData, &bid)
-	if err != nil {
-		return err
-	}
-
-	cotacao := fmt.Sprintf("Dolar: %s", bid)
-
-	file, err := os.OpenFile("cotacao.txt", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
-	if err != nil {
-		fmt.Println("Um erro ocorreu ao tentar abrir/criar o arquivo")
-		return err
-	}
-	defer file.Close()
-
-	_, err = file.WriteString(cotacao)
-	if err != nil {
-		fmt.Println("Um erro ocorreu ao tentar salvar os dados no arquivo")
-		return err
-	}
-
-	fmt.Printf("Cotacao salva no arquivo com sucesso!\n")
-
-	return nil
 }
--- server/server.go
+++ server/server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -68,30 +69,36 @@ func RequestRates() (Response, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
 	defer cancel()
 
-	client := &http.Client{}
-	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
-	if err != nil {
-		return Response{}, err
-	}
+	select {
+	case <-ctx.Done():
+		fmt.Printf("%s in RequestRates\n", ctx.Err())
+		return Response{}, ctx.Err()
+	default:
+		client := &http.Client{}
+		req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+		if err != nil {
+			return Response{}, err
+		}
 
-	res, err := client.Do(req)
-	if err != nil {
-		return Response{}, err
-	}
-	defer res.Body.Close()
+		res, err := client.Do(req)
+		if err != nil {
+			return Response{}, err
+		}
+		defer res.Body.Close()
 
-	responseData, err := io.ReadAll(res.Body)
-	if err != nil {
-		return Response{}, err
-	}
+		responseData, err := io.ReadAll(res.Body)
+		if err != nil {
+			return Response{}, err
+		}
 
-	var rateData Response
-	err = json.Unmarshal(responseData, &rateData)
-	if err != nil {
-		return Response{}, err
-	}
+		var rateData Response
+		err = json.Unmarshal(responseData, &rateData)
+		if err != nil {
+			return Response{}, err
+		}
 
-	return rateData, nil
+		return rateData, nil
+	}
 }
 
 func saveRates(rates UsdBrlRate) error {
@@ -99,15 +106,20 @@ func saveRates(rates UsdBrlRate) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
 	defer cancel()
 
-	// open the db connection
-	db, err := sql.Open("sqlite3", "./db/mydb.db")
-	if err != nil {
-		return err
-	}
-	defer db.Close()
+	select {
+	case <-ctx.Done():
+		fmt.Printf("%s in saveRates\n", ctx.Err())
+		return ctx.Err()
+	default:
+		// open the db connection
+		db, err := sql.Open("sqlite3", "./db/mydb.db")
+		if err != nil {
+			return err
+		}
+		defer db.Close()
 
-	// creates the table if it doesn't exist
-	createTableSQL := `CREATE TABLE IF NOT EXISTS UsdBrlRate (
+		// creates the table if it doesn't exist
+		createTableSQL := `CREATE TABLE IF NOT EXISTS UsdBrlRate (
         Code TEXT,
         Codein TEXT,
         Name TEXT,
@@ -121,40 +133,41 @@ func saveRates(rates UsdBrlRate) error {
         CreateDate TEXT
     );`
 
-	err = createTable(ctx, db, createTableSQL)
-	if err != nil {
-		return err
-	}
+		err = createTable(ctx, db, createTableSQL)
+		if err != nil {
+			return err
+		}
 
-	// prepare the statement to insert the data into the table
-	insertSQL := `INSERT INTO UsdBrlRate (Code, Codein, Name, High, Low, VarBid, PctChange, Bid, Ask, Timestamp, CreateDate) 
+		// prepare the statement to insert the data into the table
+		insertSQL := `INSERT INTO UsdBrlRate (Code, Codein, Name, High, Low, VarBid, PctChange, Bid, Ask, Timestamp, CreateDate) 
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
 
-	stmt, err := db.Prepare(insertSQL)
-	if err != nil {
-		return err
-	}
+		stmt, err := db.Prepare(insertSQL)
+		if err != nil {
+			return err
+		}
 
-	// insert the data into the table
-	_, err = stmt.ExecContext(
-		ctx,
-		rates.Code,
-		rates.Codein,
-		rates.Name,
-		rates.High,
-		rates.Low,
-		rates.VarBid,
-		rates.PctChange,
-		rates.Bid,
-		rates.Ask,
-		rates.Timestamp,
-		rates.CreateDate,
-	)
-	if err != nil {
-		return err
-	}
+		// insert the data into the table
+		_, err = stmt.ExecContext(
+			ctx,
+			rates.Code,
+			rates.Codein,
+			rates.Name,
+			rates.High,
+			rates.Low,
+			rates.VarBid,
+			rates.PctChange,
+			rates.Bid,
+			rates.Ask,
+			rates.Timestamp,
+			rates.CreateDate,
+		)
+		if err != nil {
+			return err
+		}
 
-	return nil
+		return nil
+	}
 }
 
 func createTable(ctx context.Context, db *sql.DB, query string) error {
